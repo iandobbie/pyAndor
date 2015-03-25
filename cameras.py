e3d8dd70bd7c093e94fd01b65a3d8de04eb9b6b9
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2015-03-25 11:15:39 +0000

    added OMXT cameras.py config file

--- cameras.py
+++ cameras.py
@@ -1,6 +1,5 @@
 ## _cameras [(label, dsp line, ip address, model)]
 cameras = [
-    ('West', 9146, 1<<0, '192.168.1.2', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    ('East', 9145, 1<<1, '192.168.1.2', 7778, 'ixon', ['Cy5', 'FITC'], [670, 518]),
+    ('West', 7197, 1<<0, '172.16.0.20', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
     ]
 camera_keys = ['label', 'serial', 'line', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
